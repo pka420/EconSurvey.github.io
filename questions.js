6969470f958fe13786bf4be7b69f6755758c5ad6
HKay111 <76726220+HKay111@users.noreply.github.com>
2023-11-09 00:23:59 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,14 @@
 questions = [
     {
-        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
+       "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    { "question": "It is necessary for the government to intervene in the economy to protect consumers.",
         "effect": {
             "econ": 10,
             "dipl": 0,
